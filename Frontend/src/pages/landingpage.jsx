86c1f697284609ff6f0dd97f32d3de527a8ac610
Sehez Babber <sehez3010@gmail.com>
2025-03-24 11:17:23 +0530

    made landing page

---
+++ Frontend/src/pages/landingpage.jsx
@@ -0,0 +1,33 @@
+import "../App.css"
+function LandingPage() {
+    return ( 
+        <div className="landingPageContainer">
+            <nav>
+                <div className="title">
+                    <h2>Zoom Clone</h2>
+                </div>
+                <div className="linkers">
+                    <p>Join as Guest</p>
+                    <p>Register</p>
+                    <div role="button">
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+            <div className="mainContainer">
+                <div className="text">
+                    <h1 style={{color:"white"}}><span style={{color:"orange"}}>Connect </span>with your Loved Ones</h1>
+                    <h3 style={{color:"white"}}>Cover a distance by zoom</h3>
+                    <div role="button">
+                        <p>Get Started</p>
+                    </div>
+                </div>
+                <div className="image">
+                    <img src="/public/mobile.png" alt="images of 2 mobiles" />
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default LandingPage;
